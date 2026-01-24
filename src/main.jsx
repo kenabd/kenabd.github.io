b1a41da1cecd09c5f17fc95a2bc2a3c2eead36e6
Kinun <kinana61@gmail.com>
2026-01-24 18:19:53 -0500

    Fix website url

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ document.documentElement.classList.add("dark");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
